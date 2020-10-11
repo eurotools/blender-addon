2332c9df010d50cf17c11c7e1a64c419035d4289
Swyter <swyterzone@gmail.com>
2020-10-11 23:17:42 +0200

    Move things around and make the EIF script work when called from the Blender UI. We can still call it standalone, easily.

--- io_scene_sphnx/export_eif.py
+++ io_scene_sphnx/export_eif.py
@@ -7,63 +7,86 @@ from pathlib import Path
 from bpy_extras.io_utils import axis_conversion
 from bpy import context
 
-#Open writer
-out = open(str(Path.home()) + "\\Desktop\\TestEIF.EIF", "w")
+def save(context,
+         filepath,
+         *,
+         use_triangles=False,
+         use_edges=True,
+         use_normals=False,
+         use_smooth_groups=False,
+         use_smooth_groups_bitflags=False,
+         use_uvs=True,
+         use_materials=True,
+         use_mesh_modifiers=True,
+         use_mesh_modifiers_render=False,
+         use_blen_objects=True,
+         group_by_object=False,
+         group_by_material=False,
+         keep_vertex_order=False,
+         use_vertex_groups=False,
+         use_nurbs=True,
+         use_selection=True,
+         use_animation=False,
+         global_matrix=None,
+         path_mode='AUTO'
+         ):
+         
+    #Open writer
+    out = open(str(Path.home()) + "\\Desktop\\TestEIF.EIF", "w")
 
-#Get scene info
-scn = bpy.context.scene 
+    #Get scene info
+    scn = bpy.context.scene 
 
-def GetMaterials():
-    MaterialIndex = 0
+    def GetMaterials():
+        MaterialIndex = 0
+            
+        out.write("*MATERIALS {\n")
         
-    out.write("*MATERIALS {\n")
-    
-    for obj in bpy.context.scene.objects:
-        for s in obj.material_slots:
-            if s.material and s.material.use_nodes:
-                for n in s.material.node_tree.nodes:
-                    if n.type == 'TEX_IMAGE':                
-                        out.write("\t*MATERIAL %d {\n" % (MaterialIndex))
-                        out.write("\t\t*NAME \"%s\"\n" % (os.path.splitext(n.image.name)[0]))                    
-                        out.write("\t\t*MAP_DIFFUSE \"%s\"\n" % (bpy.path.abspath(n.image.filepath)))      
-                        #Check if the texture exists
-                        if (os.path.exists(bpy.path.abspath(n.image.filepath))):
-                            out.write("\t\t*TWOSIDED True\n")
-                        else:
-                            out.write("\t\t*TWOSIDED False\n")
-                        out.write("\t\t*MAP_DIFFUSE_AMOUNT 1.0\n")
-                        
-                        #Add 1 to the materials index
-                        MaterialIndex +=1
-    out.write("\t}\n")
-    out.write("}\n\n")
-    
-def GetMesh():
-    for ob in scn.objects:
-        if ob.type == 'MESH':
-            me = ob.data
-            uv_layer = me.uv_layers.active.data
-              
-            out.write("*MESH {\n")    
-            out.write("\t*NAME %s\n" % (me.name))
-            out.write("\t*VERTCOUNT %d\n" % (len(me.vertices)))
-            out.write("\t*FACECOUNT %d\n" % (len(me.polygons)))
-            out.write("\t*FACELAYERSCOUNT 1\n")
-            out.write("\t*FACESHADERCOUNT 1\n")
-            out.write("\t*VERTEX_LIST {\n")
-            for v in ob.data.vertices:
-                out.write("\t\t%.6f %.6f %.6f\n" % (v.co.x, v.co.y, v.co.z))
-            out.write("\t}\n")
-            out.write("\t*UV_LIST {\n")            
-            for poly in me.polygons:
-                for loop_index in range(poly.loop_start, poly.loop_start + poly.loop_total):
-                    out.write("\t\t%.6f %.6f\n" % (uv_layer[loop_index].uv.x, uv_layer[loop_index].uv.y))
-            out.write("\t}\n")
-            out.write("\t*VERTCOL_LIST {\n")
+        for obj in bpy.context.scene.objects:
+            for s in obj.material_slots:
+                if s.material and s.material.use_nodes:
+                    for n in s.material.node_tree.nodes:
+                        if n.type == 'TEX_IMAGE':                
+                            out.write("\t*MATERIAL %d {\n" % (MaterialIndex))
+                            out.write("\t\t*NAME \"%s\"\n" % (os.path.splitext(n.image.name)[0]))                    
+                            out.write("\t\t*MAP_DIFFUSE \"%s\"\n" % (bpy.path.abspath(n.image.filepath)))      
+                            #Check if the texture exists
+                            if (os.path.exists(bpy.path.abspath(n.image.filepath))):
+                                out.write("\t\t*TWOSIDED True\n")
+                            else:
+                                out.write("\t\t*TWOSIDED False\n")
+                            out.write("\t\t*MAP_DIFFUSE_AMOUNT 1.0\n")
+                            
+                            #Add 1 to the materials index
+                            MaterialIndex +=1
+        out.write("\t}\n")
+        out.write("}\n\n")
+        
+    def GetMesh():
+        for ob in scn.objects:
+            if ob.type == 'MESH':
+                me = ob.data
+                uv_layer = me.uv_layers.active.data
+                  
+                out.write("*MESH {\n")    
+                out.write("\t*NAME %s\n" % (me.name))
+                out.write("\t*VERTCOUNT %d\n" % (len(me.vertices)))
+                out.write("\t*FACECOUNT %d\n" % (len(me.polygons)))
+                out.write("\t*FACELAYERSCOUNT 1\n")
+                out.write("\t*FACESHADERCOUNT 1\n")
+                out.write("\t*VERTEX_LIST {\n")
+                for v in ob.data.vertices:
+                    out.write("\t\t%.6f %.6f %.6f\n" % (v.co.x, v.co.y, v.co.z))
+                out.write("\t}\n")
+                out.write("\t*UV_LIST {\n")            
+                for poly in me.polygons:
+                    for loop_index in range(poly.loop_start, poly.loop_start + poly.loop_total):
+                        out.write("\t\t%.6f %.6f\n" % (uv_layer[loop_index].uv.x, uv_layer[loop_index].uv.y))
+                out.write("\t}\n")
+                out.write("\t*VERTCOL_LIST {\n")
 
-            out.write("\t}\n")
-    
-def write_file():
+                out.write("\t}\n")
+
     #Script header
     out.write("*EUROCOM_INTERCHANGE_FILE 100\n")
     out.write("*COMMENT Eurocom Interchange File Version 1.00 Monday January 06 2003 12:13\n\n")
@@ -85,7 +108,8 @@ def write_file():
     
     #Write Meshes
     GetMesh()
-write_file()
+    
+    #Close writer
+    out.close()
 
-#Close writer
-out.close()
+    return {'FINISHED'}
