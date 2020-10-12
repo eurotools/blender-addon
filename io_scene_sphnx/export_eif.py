1b26c9056f9d1b95535cdf3b5943cb967a1abb66
Swyter <swyterzone@gmail.com>
2020-10-13 01:23:24 +0200

    Change generated EIF tabs into two spaces for readability, correctly use *TWOSIDED, it is true when present, false when missing. Other small tweaks.

--- io_scene_sphnx/export_eif.py
+++ io_scene_sphnx/export_eif.py
@@ -53,20 +53,18 @@ def save(context,
                     DiffuseColor = s.material.diffuse_color
                     for n in s.material.node_tree.nodes:
                         if n.type == 'TEX_IMAGE':                
-                            ow("\t*MATERIAL %d {\n" % (MaterialIndex))
-                            ow("\t\t*NAME \"%s\"\n" % (os.path.splitext(n.image.name)[0]))
-                            ow("\t\t*COL_DIFFUSE %.6f %.6f %.6f\n" % (DiffuseColor[0], DiffuseColor[1], DiffuseColor[2]))
-                            ow("\t\t*MAP_DIFFUSE \"%s\"\n" % (bpy.path.abspath(n.image.filepath)))      
+                            ow("  *MATERIAL %d {\n" % (MaterialIndex))
+                            ow("    *NAME \"%s\"\n" % (os.path.splitext(n.image.name)[0]))
+                            ow("    *COL_DIFFUSE %.6f %.6f %.6f\n" % (DiffuseColor[0], DiffuseColor[1], DiffuseColor[2]))
+                            ow("    *MAP_DIFFUSE \"%s\"\n" % (bpy.path.abspath(n.image.filepath)))      
                             #Check if the texture exists
                             if (os.path.exists(bpy.path.abspath(n.image.filepath))):
-                                ow("\t\t*TWOSIDED True\n")
-                            else:
-                                ow("\t\t*TWOSIDED False\n")
-                            ow("\t\t*MAP_DIFFUSE_AMOUNT 1.0\n")
+                                ow("    *TWOSIDED\n")
+                            ow("    *MAP_DIFFUSE_AMOUNT 1.0\n")
                             
                             #Add 1 to the materials index
                             MaterialIndex +=1
-        ow("\t}\n")
+        ow("  }\n")
         ow("}\n\n")
         
     def GetMesh():
@@ -102,57 +100,57 @@ def save(context,
                         
                 #==================PRINT DATA==============================
                 ow("*MESH {\n")    
-                ow("\t*NAME \"%s\"\n" % (me.name))
-                ow("\t*VERTCOUNT %d\n" % (len(VertexList)))
-                ow("\t*UVCOUNT %d\n" % (len(UVList)))
+                ow("  *NAME \"%s\"\n" % (me.name))
+                ow("  *VERTCOUNT %d\n" % (len(VertexList)))
+                ow("  *UVCOUNT %d\n" % (len(UVList)))
                 if(len(VertColList) > 0):
-                    ow("\t*VERTCOLCOUNT %d\n" % (len(VertColList)))
-                ow("\t*FACECOUNT %d\n" % (len(me.polygons)))
-                ow("\t*TRIFACECOUNT %d\n" % (total_triangles))
-                ow("\t*FACELAYERSCOUNT %d\n" % len(me.uv_layers))
+                    ow("  *VERTCOLCOUNT %d\n" % (len(VertColList)))
+                ow("  *FACECOUNT %d\n" % (len(me.polygons)))
+                ow("  *TRIFACECOUNT %d\n" % (total_triangles))
+                ow("  *FACELAYERSCOUNT %d\n" % len(me.uv_layers))
                 
                 #Check if there are more than one layer
                 if (len(me.uv_layers) > 1):
-                    ow("\t*FACESHADERCOUNT %d\n" % len(me.uv_layers))
+                    ow("  *FACESHADERCOUNT %d\n" % len(me.uv_layers))
                 
                 #Print Vertex data
-                ow("\t*VERTEX_LIST {\n")
+                ow("  *VERTEX_LIST {\n")
                 for list_item in VertexList:
                     dataSplit = list_item.split(",")
-                    ow("\t\t%s %s %s\n" % (dataSplit[0], dataSplit[1], dataSplit[2]))
-                ow("\t}\n")
+                    ow("    %s %s %s\n" % (dataSplit[0], dataSplit[1], dataSplit[2]))
+                ow("  }\n")
                 
                 #Print UV data
-                ow("\t*UV_LIST {\n")
+                ow("  *UV_LIST {\n")
                 for list_item in UVList:
                     dataSplit = list_item.split(",")
-                    ow("\t\t%s %s\n" % (dataSplit[0], dataSplit[1]))
-                ow("\t}\n")
+                    ow("    %s %s\n" % (dataSplit[0], dataSplit[1]))
+                ow("  }\n")
                 
                 #Check if the vertex colors layer is active
                 if(len(VertColList) > 0):
-                    ow("\t*VERTCOL_LIST {\n")
+                    ow("  *VERTCOL_LIST {\n")
                     for list_item in VertColList:
                         dataSplit = list_item.split(",")
-                        ow("\t\t%s %s %s %s\n" % (dataSplit[0], dataSplit[1], dataSplit[2], dataSplit[3]))                    
-                    ow("\t}\n")
+                        ow("    %s %s %s %s\n" % (dataSplit[0], dataSplit[1], dataSplit[2], dataSplit[3]))                    
+                    ow("  }\n")
                     
                 #Print Shader faces
                 if (len(me.uv_layers) > 1):
-                    ow("\t*FACESHADERS {\n")
-                    ow("\t}\n")
-                ow("\t*FACEFORMAT VT\n")
+                    ow("  *FACESHADERS {\n")
+                    ow("  }\n")
+                ow("  *FACEFORMAT VT\n")
                 
                 CalcIndex = 0
                 
                 #Print Face list
-                ow("\t*FACE_LIST {\n")
+                ow("  *FACE_LIST {\n")
                 for poly in me.polygons:
                     #Get polygon vertices
                     PolygonVertices = poly.vertices
                     TotalIndexVertex = []
                     #Write vertices
-                    ow("\t\t%d " % (len(PolygonVertices)))
+                    ow("    %d " % (len(PolygonVertices)))
                     for vert in PolygonVertices:
                         TotalIndexVertex.append(vert) 
                         ow("%d " % vert)
@@ -161,7 +159,7 @@ def save(context,
                         ow("%d " % CalcIndex)
                         CalcIndex += 1
                     ow("\n")
-                ow("\t}\n")
+                ow("  }\n")
                 
                 #Close Tag
                 ow("}\n")
@@ -171,22 +169,18 @@ def save(context,
     #Script header
     ow("*EUROCOM_INTERCHANGE_FILE 100\n")
     ow("*COMMENT Eurocom Interchange File Version 1.00 %s\n\n" % time_now.strftime("%A %B %d %Y %H:%M"))
-    ow("*OPTIONS {\n")
-    ow("\t*COORD_SYSTEM LH\n")
-    ow("}\n\n")
 
     #print scene info
     ow("*SCENE {\n")
-    ow("\t*FILENAME \"%s\"\n" % (os.path.abspath(r'C:\Users\Jordi Martinez\Desktop\Sphinx and the shadow of set\CutScenes\Abydos\CS_Aby_South.elf')))
-    ow("\t*FIRSTFRAME %d\n" % (scn.frame_start))
-    ow("\t*LASTFRAME %d\n" % (scn.frame_end))
-    ow("\t*FRAMESPEED %d\n" % (scn.render.fps))  
-    ow("\t*STATICFRAME 0\n")
-    ow("\t*AMBIENTSTATIC 1.0 1.0 1.0\n")
+    ow("  *FIRSTFRAME %d\n" % (scn.frame_start))
+    ow("  *LASTFRAME %d\n"  % (scn.frame_end  ))
+    ow("  *FRAMESPEED %d\n" % (scn.render.fps ))
+    ow("  *STATICFRAME 0\n")
+    ow("  *AMBIENTSTATIC 1.0 1.0 1.0\n")
     ow("}\n\n")
 
     #Write materials
-#    GetMaterials()
+#   GetMaterials()
 
     #Write Meshes
     GetMesh()
@@ -198,4 +192,4 @@ def save(context,
     
     
 if __name__ == "__main__":
-    save({}, str(Path.home()) + "\\Desktop\\TestEIF.eif")
+    save({}, str(Path.home()) + "/Desktop/testEIF_b.eif")
