a555901c5f813463facf9e420d988feecede2d9c
jmarti856 <jmarti856@gmail.com>
2021-02-12 21:55:44 +0100

    ESE Exporter: Finished the "NODE_TM" section

--- io_scene_sphnx/export_ese.py
+++ io_scene_sphnx/export_ese.py
@@ -14,18 +14,28 @@ from math import *
 from pathlib import Path
 from bpy_extras.io_utils import axis_conversion 
 
-def write():
+
+#===============================================================================================
+#  Main Method
+#===============================================================================================
+def WriteFile():
+    #===============================================================================================
+    #  Global Vars
+    #===============================================================================================
+    global_matrix = Matrix(((1, 0, 0),(0, 0, 1),(0, 1, 0)))
     ProjectContextScene = bpy.context.scene
-
-
-    # Stop edit mode
-    if bpy.ops.object.mode_set.poll():
-        bpy.ops.object.mode_set(mode='OBJECT')
-            
+                  
     #===============================================================================================
     #  WRITE HEADER
     #===============================================================================================
+    # Stop edit mode
+    if bpy.ops.object.mode_set.poll():
+        bpy.ops.object.mode_set(mode='OBJECT')
+    
+    #Create new file
     out = open(str(Path.home()) + "\\Desktop\\CameraExporter.ESE", "w")
+    
+    #Start writting
     out.write('*3DSMAX_EUROEXPORT	300\n')
     out.write('*COMMENT "Eurocom Export Version  3.00" - %s\n' % (datetime.datetime.utcnow()).strftime('%A %B %d %H:%M:%S %Y'))
     out.write('*COMMENT "Version of Blender that output this file: %s\n' % bpy.app.version_string)
@@ -55,6 +65,12 @@ def write():
     #=============================================================================================== 
     for SceneObj in ProjectContextScene.objects:
         if SceneObj.type == 'CAMERA':
+            
+            CameraMatrixRot = SceneObj.rotation_euler.to_matrix()
+            RotationMatrix = global_matrix @ CameraMatrixRot
+            RotationMatrix = global_matrix @ RotationMatrix.transposed()
+            
+            #---------------------------------------------[Write Data To File]---------------------------------------------
             out.write('*CAMERAOBJECT {\n')
             out.write('\t*NODE_NAME "%s"\n' % SceneObj.name)
             out.write('\t*CAMERA_TYPE %s\n' % "Target")
@@ -63,9 +79,16 @@ def write():
             out.write('\t\t*INHERIT_POS "%d %d %d"\n' % (0, 0, 0))
             out.write('\t\t*INHERIT_ROT "%d %d %d"\n' % (0, 0, 0))
             out.write('\t\t*INHERIT_SCL "%d %d %d"\n' % (1, 1, 1))    
-    
-    
+            out.write('\t\t*TM_ROW0 %.4f %.4f %.4f\n' % (RotationMatrix[0].x, RotationMatrix[0].y, RotationMatrix[0].z))
+            out.write('\t\t*TM_ROW1 %.4f %.4f %.4f\n' % (RotationMatrix[1].x, RotationMatrix[1].y, RotationMatrix[1].z))
+            out.write('\t\t*TM_ROW2 %.4f %.4f %.4f\n' % (RotationMatrix[2].x, RotationMatrix[2].y, RotationMatrix[2].z))
+            out.write('\t\t*TM_ROW3 %.4f %.4f %.4f\n' % (SceneObj.location.x, SceneObj.location.y,SceneObj.location.z))
+            out.write('\t\t*TM_POS %.4f %.4f %.4f\n' % (SceneObj.location.x, SceneObj.location.y,SceneObj.location.z))
+            out.write('\t\t*TM_ROTANGLE %.4f %.4f %.4f\n' % (math.radians(SceneObj.rotation_euler.x), math.radians(SceneObj.rotation_euler.y),math.radians(SceneObj.rotation_euler.z)))
+            out.write('\t\t*TM_SCALE %.4f %.4f %.4f\n' % (SceneObj.scale.x, SceneObj.scale.y,SceneObj.scale.z))            
+            out.write('\t\t*TM_SCALEANGLE %.4f %.4f %.4f\n' % (0, 0, 0))
+            out.write('\t}\n')
     
     #Close File
     out.close()
-write()
+WriteFile()
